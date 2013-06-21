c6bbf07836d1954eacba9c74336484ac240edfd5
Gavin M. Roy <gmr@meetme.com>
2013-06-21 12:35:30 -0400

    Bump the rev and add empty /var/run/newrelic and /var/log/newrelic directories

--- setup.py
+++ setup.py
@@ -1,17 +1,29 @@
 from setuptools import setup
 import os
+from os import path
 
 base_path = '/opt/newrelic_plugin_agent'
-
 data_files = dict()
 data_files[base_path] = ['README.md']
-for data_path in ['etc']:
-    for dir_path, dir_names, file_names in os.walk(data_path):
-        install_path = '%s/%s' % (base_path, dir_path)
-        if install_path not in data_files:
-            data_files[install_path] = list()
-        for file_name in file_names:
-            data_files[install_path].append('%s/%s' % (dir_path, file_name))
+
+# etc dir
+for dir_path, dir_names, file_names in os.walk('etc'):
+    install_path = '%s/%s' % (base_path, dir_path)
+    if install_path not in data_files:
+        data_files[install_path] = list()
+    for file_name in file_names:
+        data_files[install_path].append('%s/%s' % (dir_path, file_name))
+
+# var dir
+for dir_path, dir_names, file_names in os.walk('var'):
+    install_path = '/%s' % dir_path
+    if dir_path not in data_files:
+        data_files[install_path] = list()
+    for file_name in file_names:
+        data_files[install_path].append('%s/%s' % (dir_path, file_name))
+
+print data_files
+
 with open('MANIFEST.in', 'w') as handle:
     for path in data_files:
         for filename in data_files[path]:
@@ -25,7 +37,7 @@ extras_require = {'mongodb': ['pymongo'],
                   'postgresql': ['psycopg2']}
 
 setup(name='newrelic_plugin_agent',
-      version='1.0.11',
+      version='1.0.12',
       description='Python based agent for collecting metrics for NewRelic',
       url='https://github.com/MeetMe/newrelic_plugin_agent',
       packages=['newrelic_plugin_agent', 'newrelic_plugin_agent.plugins'],
@@ -36,4 +48,15 @@ setup(name='newrelic_plugin_agent',
       data_files=[(key, data_files[key]) for key in data_files.keys()],
       install_requires=install_requires,
       extras_require=extras_require,
-      tests_require=tests_require)
+      tests_require=tests_require,
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: POSIX',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 2 :: Only',
+          'Topic :: System :: Monitoring'])
