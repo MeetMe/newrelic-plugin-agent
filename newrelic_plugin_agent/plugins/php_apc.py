d922567a290da30dceee9d9938cfd041904006a2
Gavin M. Roy <gmr@meetme.com>
2013-07-22 18:51:14 -0400

    Use JSONStatsPlugin

--- newrelic_plugin_agent/plugins/php_apc.py
+++ newrelic_plugin_agent/plugins/php_apc.py
@@ -10,14 +10,14 @@ from newrelic_plugin_agent.plugins import base
 
 LOGGER = logging.getLogger(__name__)
 
-class APC(base.Plugin):
+class APC(base.JSONStatsPlugin):
 
     GUID = 'com.meetme.newrelic_php_apc_agent'
 
     def add_datapoints(self, stats):
         """Add all of the data points for a node
 
-        :param str stats: The stats content from APC as a string
+        :param dict stats: The stats content from APC as a string
 
         """
         shared_memory = stats.get('shared_memory', dict())
@@ -51,54 +51,3 @@ class APC(base.Plugin):
         self.add_derive_value('User Cache/Misses', '', misses)
         self.add_derive_value('User Cache/Inserts', '',
                               user_stats.get('num_inserts', 0))
-
-    @property
-    def apc_stats_url(self):
-        if 'scheme' not in self.config:
-            self.config['scheme'] = 'http'
-        return '%(scheme)s://%(host)s:%(port)s%(path)s?auto' % self.config
-
-    def fetch_data(self):
-        """Fetch the data from the APC script
-
-        :rtype: str
-
-        """
-        kwargs = {'url': self.apc_stats_url,
-                  'verify': self.config.get('verify_ssl_cert', True)}
-        if 'username' in self.config and 'password' in self.config:
-            kwargs['auth'] = (self.config['username'], self.config['password'])
-
-        try:
-            response = requests.get(**kwargs)
-        except requests.ConnectionError as error:
-            LOGGER.error('Error polling APC Stats: %s', error)
-            return {}
-
-        if response.status_code == 200:
-            try:
-                return response.json()
-            except Exception as error:
-                LOGGER.error('JSON decoding error: %r', error)
-                return ''
-        LOGGER.error('Error response from %s (%s): %s', self.apc_stats_url,
-                     response.status_code, response.content)
-        return ''
-
-    def poll(self):
-        LOGGER.info('Polling APC Stats via %s', self.apc_stats_url)
-        start_time = time.time()
-        if 'scheme' not in self.config:
-            self.config['scheme'] = 'http'
-        self.derive = dict()
-        self.gauge = dict()
-        self.rate = dict()
-        data = self.fetch_data()
-        if data:
-            self.add_datapoints(data)
-            LOGGER.info('Polling complete in %.2f seconds',
-                        time.time() - start_time)
-        else:
-            LOGGER.error('No data was returned from APC. Ensure '
-                         'configuration is correct and that %s is reachable '
-                         'by the agent', self.apc_stats_url)
