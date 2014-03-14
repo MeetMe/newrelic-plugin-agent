c6d60cb4eb5161662ec21dcf93efcfac0d44be8e
Gavin M. Roy <gavinmroy@gmail.com>
2014-03-14 00:31:21 -0400

    Add metric units for better graphing

--- newrelic_plugin_agent/plugins/pgbouncer.py
+++ newrelic_plugin_agent/plugins/pgbouncer.py
@@ -16,50 +16,54 @@ class PgBouncer(postgresql.PostgreSQL):
 
     def add_pgbouncer_stats(self, stats):
 
-        self.add_gauge_value('Overview/Databases', '',
+        self.add_gauge_value('Overview/Databases', 'databases',
                              stats['LISTS']['databases'])
-        self.add_gauge_value('Overview/Pools', '',
+        self.add_gauge_value('Overview/Pools', 'pools',
                              stats['LISTS']['pools'])
-        self.add_gauge_value('Overview/Users', '',
+        self.add_gauge_value('Overview/Users', 'users',
                              stats['LISTS']['users'])
 
-        self.add_gauge_value('Overview/Clients/Free', '',
+        self.add_gauge_value('Overview/Clients/Free', 'clients',
                              stats['LISTS']['free_clients'])
-        self.add_gauge_value('Overview/Clients/Used', '',
+        self.add_gauge_value('Overview/Clients/Used', 'clients',
                              stats['LISTS']['used_clients'])
-        self.add_gauge_value('Overview/Servers/Free', '',
+        self.add_gauge_value('Overview/Servers/Free', 'servers',
                              stats['LISTS']['free_servers'])
-        self.add_gauge_value('Overview/Servers/Used', '',
+        self.add_gauge_value('Overview/Servers/Used', 'servers',
                              stats['LISTS']['used_servers'])
 
+        requests = 0
         for database in stats['STATS']:
             metric = 'Database/%s' % database['database']
-            self.add_derive_value('%s/Query Time' % metric, 'sec',
+            self.add_derive_value('%s/Query Time' % metric, 'seconds',
                                   database['total_query_time'])
-            self.add_derive_value('%s/Requests' % metric, '',
+            self.add_derive_value('%s/Requests' % metric, 'requests',
                                   database['total_requests'])
             self.add_derive_value('%s/Data Sent' % metric, 'bytes',
                                   database['total_sent'])
             self.add_derive_value('%s/Data Received' % metric, 'bytes',
                                   database['total_received'])
+            requests += database['total_requests']
+
+        self.add_derive_value('Overview/Requests', 'requests', requests)
 
         for pool in stats['POOLS']:
             metric = 'Pools/%s' % pool['database']
-            self.add_gauge_value('%s/Clients/Active' % metric, '',
+            self.add_gauge_value('%s/Clients/Active' % metric, 'clients',
                                  pool['cl_active'])
-            self.add_gauge_value('%s/Clients/Waiting' % metric, '',
+            self.add_gauge_value('%s/Clients/Waiting' % metric, 'clients',
                                  pool['cl_waiting'])
-            self.add_gauge_value('%s/Servers/Active' % metric, '',
+            self.add_gauge_value('%s/Servers/Active' % metric, 'servers',
                                  pool['sv_active'])
-            self.add_gauge_value('%s/Servers/Idle' % metric, '',
+            self.add_gauge_value('%s/Servers/Idle' % metric, 'servers',
                                  pool['sv_idle'])
-            self.add_gauge_value('%s/Servers/Login' % metric, '',
+            self.add_gauge_value('%s/Servers/Login' % metric, 'servers',
                                  pool['sv_login'])
-            self.add_gauge_value('%s/Servers/Tested' % metric, '',
+            self.add_gauge_value('%s/Servers/Tested' % metric, 'servers',
                                  pool['sv_tested'])
-            self.add_gauge_value('%s/Servers/Used' % metric, '',
+            self.add_gauge_value('%s/Servers/Used' % metric, 'servers',
                                  pool['sv_used'])
-            self.add_gauge_value('%s/Maximum Wait' % metric, 'sec',
+            self.add_gauge_value('%s/Maximum Wait' % metric, 'seconds',
                                  pool['maxwait'])
 
     def add_stats(self, cursor):
--- newrelic_plugin_agent/plugins/php_fpm.py
+++ newrelic_plugin_agent/plugins/php_fpm.py
@@ -19,25 +19,25 @@ class FPM(base.JSONStatsPlugin):
         :param dict stats: Stats from php-fpm for a pool
 
         """
-        self.add_derive_value('Connections/Accepted', '',
+        self.add_derive_value('Connections/Accepted', 'connections',
                               stats.get('accepted conn', 0))
 
-        self.add_gauge_value('Connections/Pending', '',
+        self.add_gauge_value('Connections/Pending', 'connections',
                              stats.get('listen queue', 0),
                              max_val=stats.get('max listen queue', 0))
 
-        self.add_gauge_value('Socket Queue', '',
+        self.add_gauge_value('Socket Queue', 'connections',
                              stats.get('listen queue len', 0))
 
-        self.add_gauge_value('Processes/Active', '',
+        self.add_gauge_value('Processes/Active', 'processes',
                              stats.get('active processes', 0),
                              max_val=stats.get('max processes', 0))
 
-        self.add_gauge_value('Processes/Idle', '',
+        self.add_gauge_value('Processes/Idle', 'processes',
                              stats.get('idle processes', 0))
 
-        self.add_derive_value('Process Limit Reached', '',
+        self.add_derive_value('Process Limit Reached', 'processes',
                               stats.get('max children reached', 0))
 
-        self.add_derive_value('Slow Requests', '',
+        self.add_derive_value('Slow Requests', 'requests',
                               stats.get('slow requests', 0))
