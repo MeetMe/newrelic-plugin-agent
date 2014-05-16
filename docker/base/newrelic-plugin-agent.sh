e0b52de9b2c24ced15e03a4032cf8a7db45cd10d
Gavin M. Roy <gavinr@aweber.com>
2014-05-16 12:12:43 -0400

    No need to leave this dangling in runit

--- docker/base/newrelic-plugin-agent.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sed -i 's/REPLACE_WITH_REAL_KEY/'$NEWRELIC_KEY'/g' /etc/newrelic/newrelic-plugin-agent.cfg
-exec /sbin/setuser newrelic /usr/local/bin/newrelic-plugin-agent -c /etc/newrelic/newrelic-plugin-agent.cfg -f
