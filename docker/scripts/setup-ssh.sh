a69dc979f1f94600b0ba3c94721182d5f214f2c1
Gavin M. Roy <gavinr@aweber.com>
2014-05-16 11:03:32 -0400

    Fix the ownership of ssh related things

--- docker/scripts/setup-ssh.sh
+++ docker/scripts/setup-ssh.sh
@@ -3,3 +3,4 @@ cp /mnt/source/docker/base/id_rsa* /home/core/.ssh/
 chmod 0600 /mnt/source/docker/base/id_rsa
 cp /mnt/source/docker/base/ssh_config /home/core/.ssh/config
 chmod 0644 /mnt/source/docker/base/id_rsa.pub
+chown -R core:core /home/core/.ssh
