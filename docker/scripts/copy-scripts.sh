d29cf600b2d94f5130ed1dbe56c5320ac1b6cb17
Gavin M. Roy <gavinr@aweber.com>
2014-05-16 11:01:00 -0400

    Ensure the core user can execute the containers script

--- docker/scripts/copy-scripts.sh
+++ docker/scripts/copy-scripts.sh
@@ -3,7 +3,10 @@ mkdir -p /home/core/bin
 rm /home/core/.bashrc
 echo "export PATH=/home/core/bin:$PATH" > /home/core/.bashrc
 cp /mnt/source/docker/scripts/* /home/core/bin
-chmod u+x /home/core/bin/*
 
 # Remove the setup scripts
 rm /home/core/bin/*.sh
+
+# Make sure the core user can execute the scripts
+chown core:core /home/core/bin/*
+chmod u+x /home/core/bin/*
