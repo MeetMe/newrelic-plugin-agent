5f9e75b8da50abdf6fa68b63a16adce2670011d4
Sébastien Lavoie <github@lavoie.sl>
2014-11-21 01:33:32 -0500

    Fix apc-nrp.php when no output.

--- apc-nrp.php
+++ apc-nrp.php
@@ -4,6 +4,6 @@
  *
  */
 Header('Content-Type: application/json');
-echo json_encode(array('system_stats' => apc_cache_info('', true),
-                       'user_stats' => apc_cache_info('user', true),
-                       'shared_memory' => apc_sma_info(true)));
+echo json_encode(array('system_stats' => apc_cache_info('', true) ?: new stdClass,
+                       'user_stats' => apc_cache_info('user', true) ?: new stdClass,
+                       'shared_memory' => apc_sma_info(true)) ?: new stdClass);
